27571a7c0a1f0099f64db4d983a24c228bee35d8
Demin Dmitry <heaven7pro@gmail.com>
2022-02-08 18:15:17 +0300

    Refactor 1.2 — change arrays union

--- src/diff.js
+++ src/diff.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 
 const buildDiffTree = (data1, data2) => {
-  const keys = _.sortBy(_.uniq([...Object.keys(data1), ...Object.keys(data2)]));
+  const keys = _.sortBy(_.union(_.keys(data1), _.keys(data2)));
   return keys.map((key) => {
     const value1 = data1[key];
     const value2 = data2[key];
