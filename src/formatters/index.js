71a105e64d3d776849264e796dfefb274f154684
Demin Dmitry <heaven7pro@gmail.com>
2022-02-08 18:52:11 +0300

    Refactor 1.6 — Replace custom JSON formatter with built-in one

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,9 +1,8 @@
-import formatJson from './json.js';
 import formatPlain from './plain.js';
 import formatStylish from './stylish.js';
 
 const FORMAT_MAPPER = {
-  json: formatJson,
+  json: (tree) => JSON.stringify(tree, null, 4),
   plain: formatPlain,
   stylish: formatStylish,
 };
